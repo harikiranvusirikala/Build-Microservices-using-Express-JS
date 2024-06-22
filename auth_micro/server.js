9498f59f552c2f08164723506d6f533d1a536db8
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-06-22 23:52:09 +0530

    Auth MicroService Setup

---
+++ auth_micro/server.js
@@ -0,0 +1,9 @@
+import express from "express"
+const app = express()
+const PORT = process.env.PORT || 5001
+
+app.get("/", (req, res) => {
+  return res.json({ message: "It's working ..." })
+})
+
+app.listen(PORT, () => console.log(`Server running of PORT ${PORT}`))
