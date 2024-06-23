81157d9a70de45b42d91f3e1a7138a4cc952fdef
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-06-23 09:46:24 +0530

    Auth MicroService cors Setup

--- auth_micro/server.js
+++ auth_micro/server.js
@@ -1,6 +1,13 @@
-import express from "express"
+import express, { urlencoded } from "express"
+import cors from "cors"
+
 const app = express()
 const PORT = process.env.PORT || 5001
+
+// * Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.get("/", (req, res) => {
   return res.json({ message: "It's working ..." })
