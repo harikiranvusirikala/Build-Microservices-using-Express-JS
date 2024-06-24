7363d5adbfa44b0cfa679b705a12012a4a50590c
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-06-24 11:00:49 +0530

    Error handling for SignUp

--- auth_micro/controller/AuthController.js
+++ auth_micro/controller/AuthController.js
@@ -3,15 +3,19 @@ import prisma from "../config/db.config.js"
 
 class AuthController {
   static async register(req, res) {
-    const payload = req.body
-    const salt = bcrypt.genSaltSync(10)
-    payload.password = bcrypt.hashSync(payload.password, salt)
+    try {
+      const payload = req.body
+      const salt = bcrypt.genSaltSync(10)
+      payload.password = bcrypt.hashSync(payload.password, salt)
 
-    const user = await prisma.user.create({
-      data: payload
-    })
+      const user = await prisma.user.create({
+        data: payload
+      })
 
-    return res.json({ payload })
+      return res.json({ message: "Account created successfully!", user })
+    } catch (error) {
+      return res.status(500).json({ message: "Something went wrong. Please try again." })
+    }
   }
 }
 
