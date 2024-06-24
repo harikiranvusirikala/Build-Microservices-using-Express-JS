356d32a6ceb91bd0511bb93e21659bdbab61026b
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-06-24 11:26:03 +0530

    User Login Setup

--- auth_micro/controller/AuthController.js
+++ auth_micro/controller/AuthController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 import prisma from "../config/db.config.js"
+import jwt from "jsonwebtoken"
 
 class AuthController {
   static async register(req, res) {
@@ -15,6 +16,43 @@ class AuthController {
       return res.json({ message: "Account created successfully!", user })
     } catch (error) {
       return res.status(500).json({ message: "Something went wrong. Please try again." })
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      const { email, password } = req.body
+
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email
+        }
+      })
+
+      if (user) {
+        // * Check both password
+        if (!bcrypt.compareSync(password, user.password))
+          return res.status(401).json({ message: "Invalid credentials." })
+
+        const payload = {
+          id: user.id,
+          name: user.name,
+          email: user.email
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "365d"
+        })
+
+        return res.json({
+          message: "Logged in successfully!",
+          access_token: `Bearer ${token}`
+        })
+      }
+
+      return res.status(401).json({ message: "Invalid credentials." })
+    } catch (error) {
+      return res.status(500).json({ message: "Something went wrong." })
     }
   }
 }
--- auth_micro/server.js
+++ auth_micro/server.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express"
 import cors from "cors"
+import "dotenv/config"
 
 const app = express()
 const PORT = process.env.PORT || 5001
