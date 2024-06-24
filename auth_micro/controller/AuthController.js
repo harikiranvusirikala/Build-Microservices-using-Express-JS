2979644366dd852d7d0bd6a7add8f54d95294306
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-06-24 10:41:28 +0530

    Encrypt password before saving to DB

--- auth_micro/controller/AuthController.js
+++ auth_micro/controller/AuthController.js
@@ -1,6 +1,15 @@
+import bcrypt from "bcrypt"
+import prisma from "../config/db.config.js"
+
 class AuthController {
   static async register(req, res) {
     const payload = req.body
+    const salt = bcrypt.genSaltSync(10)
+    payload.password = bcrypt.hashSync(payload.password, salt)
+
+    const user = await prisma.user.create({
+      data: payload
+    })
 
     return res.json({ payload })
   }
