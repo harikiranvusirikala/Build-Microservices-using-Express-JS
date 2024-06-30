8b9a9b21cf1a935ef135a8c683dd051cf218918c
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-06-30 22:05:53 +0530

    Optimize the fetching users

--- post_micro/controller/PostController.js
+++ post_micro/controller/PostController.js
@@ -7,15 +7,39 @@ class PostController {
       const posts = await prisma.post.findMany({});
 
       // * Method 1 - to get user details from Auth Micro for each Post
+      // let postWithUsers = await Promise.all(
+      //   posts.map(async (post) => {
+      //     const res = await axios.get(
+      //       `${process.env.AUTH_MICRO_URL}/api/getUser/${post.user_id}`
+      //     );
+      //     console.log("the user res", res.data);
+      //     return {
+      //       ...post,
+      //       ...res.data,
+      //     };
+      //   })
+      // );
+
+      // * Method 2
+      let userIds = [];
+      posts.forEach((item) => {
+        userIds.push(item.user_id);
+      });
+
+      //   Fetch users
+      const response = await axios.post( 
+        `${process.env.AUTH_MICRO_URL}/api/getUsers`,
+        userIds
+      );
+
+      const users = response.data.users;
+
       let postWithUsers = await Promise.all(
-        posts.map(async (post) => {
-          const res = await axios.get(
-            `${process.env.AUTH_MICRO_URL}/api/getUser/${post.user_id}`
-          );
-          console.log("the user res", res.data);
+        posts.map((post) => {
+          const user = response.data.users.find((item) => item.id === post.user_id);
           return {
             ...post,
-            ...res.data,
+            user,
           };
         })
       );
